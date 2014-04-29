270a9d4da10ac683b6fa331d6274fac3b2bcf1fa
nixsiow <nixsiow@hotmail.com>
2014-04-29 12:00:37 +1000

    initial commit burning airline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BurningAirline::Application.config.secret_key_base = 'f39233b0ec831b900d1728f82e29e00cf56ec1d4c5648c64978a7fd142648af042f76f26e8a32c8e0b1852d977aefffadbdd3932a63e5bf4a183b9176b27f276'
