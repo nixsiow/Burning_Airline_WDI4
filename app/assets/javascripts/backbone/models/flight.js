f4b4a8914d6d9ef8adff325886869691245dab69
Erik Froese <froesecom@gmail.com>
2014-05-01 09:33:00 +1000

    getting close with seats view

--- app/assets/javascripts/backbone/models/flight.js
+++ app/assets/javascripts/backbone/models/flight.js
@@ -2,6 +2,7 @@ BurningAirline.Models.Flight = Backbone.Model.extend({
   urlRoot: '/flights',
   initialize: function () {
     this.airplane = new BurningAirline.Models.Airplane(this.get('airplane'));
+    debugger;
     this.set('seatCount', this.airplane.seatCount());
   }
 });
